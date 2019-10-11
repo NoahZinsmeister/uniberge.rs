395ee9f509eedbcabcf98a1007989ecf44de47fb
Noah Zinsmeister <noahwz@gmail.com>
2019-10-11 18:18:57 +0900

    move css to _app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,8 @@ import Head from 'next/head'
 import Layout from '../components/Layout'
 import useTheme from '../theme'
 
+// css reset adapted from https://hankchizljaw.com/wrote/a-modern-css-reset/
+
 function AppFunctionComponent({ Component, pageProps }) {
   const theme = useTheme()
 
@@ -16,9 +18,59 @@ function AppFunctionComponent({ Component, pageProps }) {
         <Component {...pageProps} />
         <style jsx global>
           {`
+            /* Box sizing rules */
+            *,
+            *::before,
+            *::after {
+              box-sizing: border-box;
+            }
+
+            /* global outline width */
+            * {
+              outline-width: thin;
+            }
+
+            /* Remove default padding */
+            ul[class],
+            ol[class] {
+              padding: 0;
+            }
+
+            /* Remove default margin */
+            body,
+            h1,
+            h2,
+            h3,
+            h4,
+            p,
+            ul[class],
+            ol[class],
+            li,
+            figure,
+            figcaption,
+            blockquote,
+            dl,
+            dd {
+              margin: 0;
+            }
+
+            /* Set core body defaults */
             body {
+              font-family: 'Roboto', sans-serif;
+              min-height: 100vh;
+              scroll-behavior: smooth;
+              text-rendering: optimizeSpeed;
+              line-height: 1.5;
               background-color: ${theme.colors.background};
               color: ${theme.colors.text};
+            }
+
+            /* Inherit fonts for inputs and buttons */
+            input,
+            button,
+            textarea,
+            select {
+              font: inherit;
             }
           `}
         </style>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,7 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-// css reset adapted from https://hankchizljaw.com/wrote/a-modern-css-reset/
-
 export default class MyDocument extends Document {
   render() {
     return (
@@ -18,62 +16,6 @@ export default class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
-        <style jsx global>
-          {`
-            /* Box sizing rules */
-            *,
-            *::before,
-            *::after {
-              box-sizing: border-box;
-            }
-
-            /* global outline width */
-            * {
-              outline-width: thin;
-            }
-
-            /* Remove default padding */
-            ul[class],
-            ol[class] {
-              padding: 0;
-            }
-
-            /* Remove default margin */
-            body,
-            h1,
-            h2,
-            h3,
-            h4,
-            p,
-            ul[class],
-            ol[class],
-            li,
-            figure,
-            figcaption,
-            blockquote,
-            dl,
-            dd {
-              margin: 0;
-            }
-
-            /* Set core body defaults */
-            body {
-              font-family: 'Roboto', sans-serif;
-              min-height: 100vh;
-              scroll-behavior: smooth;
-              text-rendering: optimizeSpeed;
-              line-height: 1.5;
-            }
-
-            /* Inherit fonts for inputs and buttons */
-            input,
-            button,
-            textarea,
-            select {
-              font: inherit;
-            }
-          `}
-        </style>
       </Html>
     )
   }

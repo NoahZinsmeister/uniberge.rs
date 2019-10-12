8f7b9edcbe30d531d64134ebe5ffa1659923e5bf
Noah Zinsmeister <noahwz@gmail.com>
2019-10-12 16:41:33 +0900

    add fromCodePoint polyfill

--- src/next.config.js
+++ src/next.config.js
@@ -4,5 +4,20 @@ module.exports = phase => ({
   env: {
     ENVIRONMENT: phase === PHASE_DEVELOPMENT_SERVER ? 'development' : 'production'
   },
+  // from https://github.com/zeit/next.js/tree/canary/examples/with-polyfills
+  webpack: function(cfg) {
+    const originalEntry = cfg.entry
+    cfg.entry = async () => {
+      const entries = await originalEntry()
+
+      if (entries['main.js'] && !entries['main.js'].includes('./client/polyfills.js')) {
+        entries['main.js'].unshift('./client/polyfills.js')
+      }
+
+      return entries
+    }
+
+    return cfg
+  },
   target: 'serverless'
 })
